b9345fbb0f35acbfc4cfcf456fe27eb79142b654
Ammar Chishti <ammarchishti80@gmail.com>
2019-07-14 12:06:42 -0400

    Add code to open up the Bing Login page and fill in the email TextField

--- BingRewardsAutomatorDesktop.py
+++ BingRewardsAutomatorDesktop.py
@@ -4,3 +4,7 @@ import time
 
 browser = webdriver.Chrome("/usr/local/bin/chromedriver")
 
+browser.get("http://shorturl.at/IRS57")
+
+email = browser.find_element_by_id("i0116")
+email.send_keys("email")
