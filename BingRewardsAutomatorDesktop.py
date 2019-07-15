1f2bc8d76a4160d9f57e2929a3bd9734bda7486d
Ammar Chishti <ammarchishti80@gmail.com>
2019-07-14 23:38:14 -0400

    Make clearer for user to enter in email address, password, and path to Chrome Webdriver

--- BingRewardsAutomatorDesktop.py
+++ BingRewardsAutomatorDesktop.py
@@ -17,19 +17,23 @@ def create_URLs() -> list:  # Creates 35 URLs to search on bing.com to earn rewa
     return list_of_URLs
 
 
-browser = webdriver.Chrome("/usr/local/bin/chromedriver")
+path_to_Chrome_Webdriver = "/usr/local/bin/chromedriver"
+bing_email_address = "joe123@hotmail.com"
+bing_password = "password123"
+
+browser = webdriver.Chrome(path_to_Chrome_Webdriver)
 
 browser.get("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1563130781&rver=6.7.6631.0&wp=MBI_SSL&wreply=https%3a%2f%2fwww.bing.com%2fsecure%2fPassport.aspx%3frequrl%3dhttps%253a%252f%252fwww.bing.com%252f%253ftoWww%253d1%2526redig%253d1B39D6E50827401881165767AF42E1D4%2526wlexpsignin%253d1%26sig%3d0651AC2328BB60D32AD5A1BA293361CF&lc=1033&id=264960&CSRFToken=b34a6229-88ed-47e6-83f3-dd676bd5591b&aadredir=1")
 
 email = browser.find_element_by_id("i0116")
-email.send_keys("your_email")
+email.send_keys(bing_email_address)
 
 submit_button = browser.find_element_by_id("idSIButton9")
 submit_button.click()
 time.sleep(1)   # Everytime you open a new webpage, you must wait a little bit for the webpage to load
 
 password = browser.find_element_by_id("i0118")
-password.send_keys("your_password")
+password.send_keys(bing_password)
 
 submit_button = browser.find_element_by_id("idSIButton9")
 submit_button.click()
